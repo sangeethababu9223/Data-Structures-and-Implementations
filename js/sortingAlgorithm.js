91281b7c5cfd271ef861226033a3400499a35caa
Nila <sangeetha9223@gmail.com>
2023-02-06 12:14:45 +0530

    Isertion sort

--- js/sortingAlgorithm.js
+++ js/sortingAlgorithm.js
@@ -28,3 +28,15 @@ function bubbleSortOpt(arr) {
   }
   return arr;
 }
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let val = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > val; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = val;
+  }
+  return arr;
+}
