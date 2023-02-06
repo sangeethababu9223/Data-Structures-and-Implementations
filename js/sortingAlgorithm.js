7be2ea26e0b138aa25aebbae05cc147563711611
Nila <sangeetha9223@gmail.com>
2023-02-06 11:24:12 +0530

    optmized bubblesort

--- js/sortingAlgorithm.js
+++ js/sortingAlgorithm.js
@@ -1,15 +1,29 @@
 function bubbleSort(arr) {
-  if (arr.length > 0) {
-    let length = arr.length - 1;
-    for (let j = 0; j < arr.length - 1; j++) {
-      for (let i = 0; i < length; i++) {
-        if (arr[i] > arr[i + 1]) {
-          let temp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = temp;
-        }
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length - j - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
       }
-      length--;
+    }
+  }
+  return arr;
+}
+function bubbleSortOpt(arr) {
+  let swapped = false;
+  for (let j = 0; j < arr.length; j++) {
+    swapped = false;
+    for (let i = 0; i < arr.length - j - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
     }
   }
   return arr;
